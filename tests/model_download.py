ca3ab70391a9fd416e4167d05cc585c5382bafaf
Andrew Moore <andrew.p.moore94@gmail.com>
2026-04-20 17:00:22 +0000

    Caching test models

---
+++ tests/model_download.py
@@ -0,0 +1,34 @@
+import argparse
+import logging
+from pathlib import Path
+
+from malaya.torch_model.t5 import T5ForTokenClassification
+from malaya_boilerplate.huggingface import download_files
+from transformers import AutoTokenizer
+
+logger = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+    description = (
+        "Downloads the pre-trained model and tokenizer that is used in testing"
+    )
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument("cache_directory", type=Path)
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging.INFO)
+
+    cache_directory = args.cache_directory
+    assert isinstance(cache_directory, Path)
+
+    models = ["mesolitica/stem-lstm-512", "mesolitica/pos-t5-small-standard-bahasa-cased"]
+    for model in models:
+        
+        logger.info(f"Caching model; {model} and tokenizer too: {cache_directory}")
+        if model == "mesolitica/pos-t5-small-standard-bahasa-cased":
+            T5ForTokenClassification.from_pretrained(model, cache_dir=cache_directory)
+        else:
+            s3_file = {'model': 'model.pt'}
+            path = download_files(model, s3_file)
+        AutoTokenizer.from_pretrained(model, cache_dir=cache_directory)
+        logger.info(f"Download; {model}")
